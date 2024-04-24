0f820977d1fe065139cbd9c57f4415f5802c5dee
momoka.sugiura <momoka.sugiura.d088@dirbato.co.jp>
2024-04-24 10:35:39 +0900

    最初のコミット

---
+++ HelloSpringBoot/src/main/java/com/techacademy/HelloSpringBootController.java
@@ -0,0 +1,35 @@
+package com.techacademy;
+
+import java.time.LocalDateTime; 
+import java.time.format.DateTimeFormatter; 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable; // 追加
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSpringBootController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello SpringBoot!";
+    }
+    
+    // ----- 追記:ここから -----
+    @GetMapping("now")
+    public String dispTime() {
+        String now = LocalDateTime.now().format( DateTimeFormatter.ofPattern("HH:mm:ss") );
+
+        return "現在時刻：" + now;
+    }
+    // ----- 追加:ここまで -----
+    
+ // ----- 追加:ここから -----
+    @GetMapping("/plus/{val1}/{val2}")
+    public String calcPlus(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 + val2;
+        return "計算結果：" + res;
+    }
+    // ----- 追加:ここまで -----
+}
